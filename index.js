df8d861393112f100626c6a67e2f03813271eb47
DATBOI <alexsimons9999@gmail.com>
2019-05-19 18:44:39 -0500

    (╯°□°)╯︵sʞɹoʍ ʇI

--- index.js
+++ index.js
@@ -7,17 +7,38 @@ const upsideDownDictionary = require('./dictionary');
 
 const flipWord = (workToFlip) => {
   let characters = workToFlip.split("");
-  return characters.map(character => upsideDownDictionary[character] || upsideDownDictionary[character.toLocaleLowerCase()])
+  return characters.map(character => upsideDownDictionary[character] || upsideDownDictionary[character.toLocaleLowerCase()] || character)
     .reduce((message, character)=> character + message, '');
 };
 
 application.use(bodyParser.json({strict: false}));
 application.use(bodyParser.urlencoded({extended: true}));
 
+const getResponse = requestBody => {
+  return '(╯°□°)╯︵' + flipWord(requestBody.text.trim().substring(5));
+};
+
+const isGoodRequest = request => {
+  const requestBody = request.body;
+  return requestBody && requestBody.text &&
+    requestBody.text.startsWith("flip")
+};
+
 application.post('/', (request, response)=>{
-  console.log(JSON.stringify(request.body));
-  response.send("Go away\n");
+  if(isGoodRequest(request)){
+    response.json({
+      "text": getResponse(request.body),
+      "response_type": "in_channel",
+    });
+  } else {
+    response.json({
+      "text": `Usage: <Command> <Argument>`,
+      "response_type": "in_channel",
+    });
+  }
 });
+
+
 
 
 application.listen(8000, ()=>console.log("listening on 8000"));
