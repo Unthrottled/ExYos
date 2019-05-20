0fbda06701822bf7a3f7dc6d14fb954086dc53dd
DATBOI <alexsimons9999@gmail.com>
2019-05-20 06:00:25 -0500

    Think these are MVP features

--- src/CommandHandler.js
+++ src/CommandHandler.js
@@ -1,9 +1,9 @@
 const {flipWord} = require('./flip/WordFlip');
 
-const extractFlipExpressionParts = command => {
-  const face = '°□°';
+const extractFlipExpressionParts = flipArguments => {
+  const face = '°□°'|| 'ಠ_ಠ' || 'ಠ益ಠ' || '◉Д◉';
   const velocity = '︵' || '彡';
-  const flippedItem = flipWord(command.substring(5)) || '┻━┻';
+  const flippedItem = flipWord(flipArguments) || '┻━┻' || '/(.□ . \\)';
   return {
     face,
     velocity,
@@ -11,29 +11,51 @@ const extractFlipExpressionParts = command => {
   }
 };
 
-const getResponse = requestBody => {
-  let command = requestBody.text.trim();
-  const {face, velocity, flippedItem } = extractFlipExpressionParts(command);
-  return `(╯${face})╯${velocity}${flippedItem}`;
+const FLIP = 'flip';
+const UNFLIP = 'unflip';
+const ZALGO = 'zalgo';
+
+const flipWordCommand = flipArguments => {
+  const {face, velocity, flippedItem} = extractFlipExpressionParts(flipArguments);
+  return `(╯${face})╯${velocity}${flippedItem}`
 };
 
-const commands =[
-  'flip',
-  'unflip',
-  'flip-table',
-  'unflip-table',
-  'zalgo',
+const extractCommand = fullCommand=> {
+  const firstSpace = fullCommand.indexOf(' ');
+  const command = fullCommand.substring(0, firstSpace);
+  const arguments = fullCommand.substring(firstSpace+1);
+  return {
+    command,
+    arguments
+  };
+};
+
+const getResponse = requestBody => {
+  const fullCommand = requestBody.text.trim();
+  const {command, arguments } = extractCommand(fullCommand);
+  switch (command) {
+    case FLIP: return flipWordCommand(arguments);
+    case UNFLIP:
+    case ZALGO:
+    default: return '¯\\_(ツ)_/¯'
+  }
+};
+
+const commands = [
+  FLIP,
+  UNFLIP,
+  ZALGO,
 ];
 
 const isGoodRequest = request => {
   const requestBody = request.body;
   return requestBody && requestBody.text &&
-    commands.findIndex(command => requestBody.text.startsWith(command)) > -1;
+    commands.findIndex(command => requestBody.text.startsWith(`${command} `)) > -1;
 };
 
 const handler = (request, response) => {
   response.append('Access-Control-Allow-Origin', '*');
-  if (isGoodRequest(request)) {
+  if (isGoodRequest(request)) { // todo: handle correct command, bad arguments
     response.json({
       "text": getResponse(request.body),
       "response_type": "in_channel",
