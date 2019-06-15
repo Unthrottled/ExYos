16ea0fca4781c52b649afda4fee5464d3b1c08e1
DATBOI <alexsimons9999@gmail.com>
2019-06-15 18:54:21 -0500

    Added a message dictonary

---
+++ src/message/MessageDictonary.ts
@@ -0,0 +1,16 @@
+const MessageDictonary = [
+  'This message was brought to you by %name%.',
+  'Courtesy of %name%.',
+  'Totally original response from %name%.',
+  '%name% wanted you to see this.',
+  '%name% spent to long thinking about sending this.',
+  'What did you say to %name%?',
+  'Did you know %name% does not have anything better to do?',
+  '%name% smashes the keys a bit too hard.',
+  'Quoth %name%, \"Nevermore\".',
+  'It was not me, it was %name%',
+  '%name% stands for robot labor rights.',
+  'I am just the messenger, %name% ordered this',
+];
+
+export default MessageDictonary;
