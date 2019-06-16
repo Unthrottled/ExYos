5ad9d5ebfdcd87ec3c3061fd591a0b2419ca7bce
DATBOI <alexsimons9999@gmail.com>
2019-06-15 19:07:27 -0500

    Added the message to the response

--- src/message/MessageDictonary.ts
+++ src/message/MessageDictonary.ts
@@ -3,14 +3,16 @@ const MessageDictonary = [
   'Courtesy of %name%.',
   'Totally original response from %name%.',
   '%name% wanted you to see this.',
+  '%name%\'s attempt at being funny.',
   '%name% spent to long thinking about sending this.',
   'What did you say to %name%?',
   'Did you know %name% does not have anything better to do?',
   '%name% smashes the keys a bit too hard.',
   'Quoth %name%, \"Nevermore\".',
-  'It was not me, it was %name%',
+  'It was not me, it was %name%.',
   '%name% stands for robot labor rights.',
-  'I am just the messenger, %name% ordered this',
+  '%name% is the reason we cannot have nice things (JK brah).',
+  'I am just the messenger, %name% ordered this.',
 ];
 
 export default MessageDictonary;
