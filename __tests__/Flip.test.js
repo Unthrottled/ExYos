fcbad8fa3ec3bd0536b60fbb85df9c572499a81c
DATBOI <alexsimons9999@gmail.com>
2019-09-01 12:17:58 -0500

    Bumped version to v2.3.0, updated changelog, and added left phrase flip test case

--- __tests__/Flip.test.js
+++ __tests__/Flip.test.js
@@ -363,6 +363,23 @@ describe('Flip Command', () => {
       expect(text).toEqual('┻━┻︵└(■_■-└)');
       expect(slackUrl).toEqual('https://hooks.slack.com/commands/1234/5678');
     });
+    it('should return left flipped phrase when given just a face argument: cool', async () => {
+      const headers = {
+        'X-Slack-Request-Timestamp': 'aoeu'
+      };
+      const request = {
+        header: header => headers[header],
+        body: {
+          ...BASE_REQUEST,
+          'text': 'flip -cool -left Thanks Obama'
+        }
+      };
+      const {exyosResponse : { attachments, response_type, text }, slackUrl} = await generateResponse(request);
+      expect(attachments[0].text).toContain('<@U2147483697>');
+      expect(response_type).toEqual("in_channel");
+      expect(text).toEqual('ɐɯɐqO sʞuɐɥ┴︵└(■_■-└)');
+      expect(slackUrl).toEqual('https://hooks.slack.com/commands/1234/5678');
+    });
 
     it('should return left flipped table when given just a face argument: pretty', async () => {
       const headers = {
